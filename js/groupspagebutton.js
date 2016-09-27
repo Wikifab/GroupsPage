90d4fcbfea65a8b2dbbe105fea1394aa5a6e9ef7
Pierre Boutet <boutetpierre@yahoo.fr>
2016-09-27 12:29:31 +0200

    groups page style

--- js/groupspagebutton.js
+++ js/groupspagebutton.js
@@ -3,18 +3,15 @@
 
 	function groupAdded( grouppage, page) {
 		$(".addToGroupLink[data-grouppage='"+grouppage+"'][data-page='"+page+"']").addClass('groupAdded').removeClass('groupRemoved');
+		$(".addToGroupLink[data-grouppage='"+grouppage+"'][data-page='"+page+"'] i").removeClass('fa-square-o').addClass('fa-check-square-o');
 	};
 	function groupRemoved(grouppage, page) {
 		$(".addToGroupLink[data-grouppage='"+grouppage+"'][data-page='"+page+"']").removeClass('groupAdded').addClass('groupRemoved');
+		$(".addToGroupLink[data-grouppage='"+grouppage+"'][data-page='"+page+"'] i").removeClass('fa-check-square-o').addClass('fa-square-o');
 	};
 
 
-
-	$('.addToGroupLink').click(function() {
-		
-		var grouppage = $(this).attr('data-grouppage');
-		var page = $(this).attr('data-page');
-		var button = this;
+	function addToGroup(grouppage, page) {
 		
 		// fonction to do second request to execute follow action
 		function ajaxGroupsPageQuery(jsondata) {
@@ -45,22 +42,16 @@
 		    dataType: 'json',
 		    success: ajaxGroupsPageQuery
 		});
-	});
-
-
-	$('.removeFromGroupsPage').click(function() {
-
-		var grouppage = $(this).attr('data-grouppage');
-		var page = $(this).attr('data-page');
-		var button = this;
-		
+	}
+	
+	function removeFromGroup(grouppage, page) {
 		// fonction to do second request to execute follow action
 		function ajaxGroupsPageQuery(jsondata) {
 			var token = jsondata.query.tokens.csrftoken;
 			$.ajax({
 				type: "POST",
 				url: mw.util.wikiScript('api'),
-				data: { action:'goupspage', format:'json', groupaction: 'remove', token: token, page: page, groupspage: grouppage},
+				data: { action:'goupspage', format:'json', groupaction: 'remove', token: token, memberpage: page, groupspage: grouppage},
 			    dataType: 'json',
 			    success: function (jsondata) {
 					if(jsondata.goupspage.success == 1) {
@@ -77,5 +68,19 @@
 		    dataType: 'json',
 		    success: ajaxGroupsPageQuery
 		});
+	}
+
+	$('.addToGroupLink').click(function() {
+		var grouppage = $(this).attr('data-grouppage');
+		var page = $(this).attr('data-page');
+
+		if ($(this).hasClass('groupAdded')) {
+			removeFromGroup(grouppage, page);
+		} else {
+			addToGroup(grouppage, page);
+		}
+		
+		
+		
 	});
 })();
